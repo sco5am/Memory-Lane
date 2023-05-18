406b3c6741ba25da0ba3af054e58d6608c264ef2
Sam Oaks <samuelclayoaks@yahoo.com>
2023-05-18 18:50:43 -0400

    set up server connection

--- server/index.js
+++ server/index.js
@@ -9,3 +9,12 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://sco5am:Dns3661dns3661@cluster0.phy88n6.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+
+// https://www.mongodb.com/cloud/atlas
